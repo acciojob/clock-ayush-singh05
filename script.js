2028041d7bdb9bcdc84fcc6f81794b453e4aeb2c
Ayush Singh <99062720+ayush-singh05@users.noreply.github.com>
2023-10-12 12:03:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+let date = new Date();
+const time = document.getElementById("timer");
+let d = date.getDate();
+
+let arr = date.toString().split(' ');
+let month = date.getMonth();
+let year = date.getFullYear();
+time.innerText = d+"/"+month+"/"+year+", "+arr[4];
